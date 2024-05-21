0c52ae5f920f713564829a26b8315e3d409eec5b
Kateryna Meleshenko <yekaterynamel@gmail.com>
2024-05-22 00:43:16 +0200

    DF name changed

--- calculations.py
+++ calculations.py
@@ -4,13 +4,13 @@ import pandas as pd
 big_mac_price = pd.read_csv('big-mac-source-data.csv')
 
 # Leave the freshest data
-big_mac_price_2024 = big_mac_price[big_mac_price['date'] == '2024-01-01']
+big_mac_price = big_mac_price[big_mac_price['date'] == '2024-01-01']
 
 # Find the max GDP among countries for further normalization
-max_GDP = big_mac_price_2024['GDP_dollar'].max() # as to 01.01.2024 it is Norway
+max_GDP = big_mac_price['GDP_dollar'].max() # as to 01.01.2024 it is Norway
 
 # Calculate BigMac prices in usd
-big_mac_price_2024['big_mac_doll'] = round(big_mac_price_2024['local_price'] / big_mac_price_2024['dollar_ex'], 2)
+big_mac_price['big_mac_doll'] = round(big_mac_price['local_price'] / big_mac_price['dollar_ex'], 2)
 
 # Carry normalization for BigMac prices 
-big_mac_price_2024['adj_doll'] = round(big_mac_price_2024['big_mac_doll'] * max_GDP/big_mac_price_2024['GDP_dollar'], 2)
+big_mac_price['adj_doll'] = round(big_mac_price['big_mac_doll'] * max_GDP / big_mac_price['GDP_dollar'], 2)
